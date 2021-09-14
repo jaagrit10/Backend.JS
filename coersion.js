453182df5d8c8170092d70f617eb30be04b9c973
Jaagrit Arora <jaagritarora.btcse20@pec.edu.in>
2021-09-14 23:54:05 +0530

    Create coersion.js

---
+++ coersion.js
@@ -0,0 +1,14 @@
+var x = null; // "null"
+console.log("" + x);
+
+var b = true;
+console.log("" + b);
+
+var nzero = -0;
+console.log("" + nzero);
+console.log(""  + []);
+console.log("" + [1,2,3])
+console.log("" + [null,undefined])
+console.log("" + [[],[],[]])
+console.log("" + [,])
+console.log("" + [,,,])
